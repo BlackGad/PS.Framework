ff134198f4ac166aa67fd929054e7dfa5efecc2d
Volodymyr Shkolka <ztBlackGad@gmail.com>
2019-07-11 23:41:47 +0300

    Added WindowsService implementation for WPF DataTemplateFactoryService moved to separated directory Added Dispatcher extensions

---
+++ PS.MVVM.WPF/Services/WindowService/IWindowService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace PS.MVVM.Services.WindowService
+{
+    public interface IWindowService
+    {
+        TViewModel Show<TViewModel>(TViewModel viewModel = default(TViewModel), string key = null);
+
+        Task<TViewModel> ShowModalAsync<TViewModel>(TViewModel viewModel = default(TViewModel), string key = null);
+    }
+}
