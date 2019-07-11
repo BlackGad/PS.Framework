fcd99de016010fff063e42346e8192b170d57ae9
Volodymyr Shkolka <ztBlackGad@gmail.com>
2019-07-12 00:46:21 +0300

    Added CloseDialogCommand implementation Fixed Assembly Xmlns definitions

--- PS.WPF/AssembyAttributes.cs
+++ PS.WPF/AssembyAttributes.cs
@@ -4,10 +4,12 @@
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.DataTemplateSelector")]
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.StyleSelector")]
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.ValueConverters.SwitchValueConverter")]
+[assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.Controls")]
+[assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.Commands")]
 [assembly: XmlnsPrefix("http://schemas.ps.com/2019/xaml", "ps")]
 
-[assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.Markup")]
-[assembly: XmlnsPrefix("http://schemas.ps.com/2019/xaml", "m")]
+[assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml/markup", "PS.WPF.Markup")]
+[assembly: XmlnsPrefix("http://schemas.ps.com/2019/xaml/markup", "m")]
 
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml/datatemplateselector", "PS.WPF.DataTemplateSelector.Rules")]
 [assembly: XmlnsPrefix("http://schemas.ps.com/2019/xaml/datatemplateselector", "dts")]
---
+++ PS.WPF/Extensions/WindowExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Windows;
+
+namespace PS.WPF.Extensions
+{
+    public static class WindowExtensions
+    {
+        #region Constants
+
+        private static readonly FieldInfo ShowingAsDialogField;
+
+        #endregion
+
+        #region Static members
+
+        public static bool IsModal(this Window window)
+        {
+            if (window == null) return false;
+            return (bool)ShowingAsDialogField.GetValue(window);
+        }
+
+        #endregion
+
+        #region Constructors
+
+        static WindowExtensions()
+        {
+            ShowingAsDialogField = typeof(Window).GetField("_showingAsDialog", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        #endregion
+    }
+}
