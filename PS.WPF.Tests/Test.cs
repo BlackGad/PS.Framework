4f2fafe0f1372565b2fe7ce4dc44b4cda43ab710
Shkolka Volodymyr <ztBlackGad@gmail.com>
2021-10-14 15:31:02 +0300

    Vleared obsolete code in test

--- PS.WPF.Tests/Test.cs
+++ PS.WPF.Tests/Test.cs
@@ -1,7 +1,4 @@
-﻿using System.Linq;
-using System.Threading;
-using System.Xml.Linq;
-using System.Xml.XPath;
+﻿using System.Threading;
 using NUnit.Framework;
 
 namespace PS.WPF.Tests
@@ -15,17 +12,6 @@ namespace PS.WPF.Tests
         [Test]
         public static void TestMethod()
         {
-            var configurationFilePath = @"C:\Projects\GitHub\BlackGad\PS.Framework\PS.WPF\Theme\ThemePalette.xml";
-            var configuration = XDocument.Load(configurationFilePath);
-            var colors = configuration.XPathSelectElements("/Colors/Color[@Name and @Name[not(.=\"\")]]").ToList();
-
-            foreach (var color in colors)
-            {
-                var name = color.Attribute(XName.Get("Name")).Value;
-                ;
-                //Generate
-            }
-
             Assert.Inconclusive();
         }
 
