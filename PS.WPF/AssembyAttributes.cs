c8c33e04a04db331a38e0275fcdeeaae7fcf786f
Shkolka Volodymyr <ztBlackGad@gmail.com>
2021-10-22 20:55:02 +0300

    Added BusyContainer to XAML ps namespace

--- PS.WPF/AssembyAttributes.cs
+++ PS.WPF/AssembyAttributes.cs
@@ -7,6 +7,7 @@
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.ValueConverters.SwitchValueConverter")]
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.ValueConverters")]
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.Controls")]
+[assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.Controls.BusyContainer")]
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.Commands")]
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.Theme")]
 [assembly: XmlnsDefinition("http://schemas.ps.com/2019/xaml", "PS.WPF.Components")]
