9a9d321e328d971015737e5c638f92678613d62d
Shkolka Volodymyr <ztBlackGad@gmail.com>
2022-09-15 10:30:19 +0300

    WIP

---
+++ .pipelines/update-version-code-script.ps1
@@ -0,0 +1,30 @@
+Param($URL, $PAT, $MODE)
+
+# Base64-encodes the Personal Access Token (PAT) appropriately
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "", $PAT)))
+
+Write-Host "The URL for this call: " $URL
+
+# Get the variable group by id
+$groupVariables = Invoke-RestMethod -Uri $URL -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+
+Write-Host "Get group variables call result: " $groupVariables
+
+# Update the necessary varible values
+switch ($MODE) 
+{
+   "MAJOR" { $groupVariables.variables.Major.value = $groupVariables.variables.Major.value/1 + 1; break; }
+   "MINOR" { $groupVariables.variables.Minor.value = $groupVariables.variables.Minor.value/1 + 1; break; }
+   "REVISION" { $groupVariables.variables.Revision.value = $groupVariables.variables.Revision.value/1 + 1; break; }
+   default { throw "Something else happened";}
+}
+
+# Convert to Json
+$json = ($groupVariables | ConvertTo-Json -Compress).ToString()
+      
+Write-Host "After modifying and Jsonifying the variables: " $json
+
+# Send the updated varible group back to Azure DevOps
+$pipeline = Invoke-RestMethod -Uri $URL -Method Put -Body $json -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+
+Write-Host "Updating variable group was successful."
