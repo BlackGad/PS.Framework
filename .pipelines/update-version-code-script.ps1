02a0bc0be808c4c72032708c5884bdead895b664
Shkolka Volodymyr <ztBlackGad@gmail.com>
2022-09-15 10:58:20 +0300

    WIP

--- .pipelines/update-version-code-script.ps1
+++ .pipelines/update-version-code-script.ps1
@@ -1,30 +1,12 @@
 Param($URL, $PAT, $MODE)
-
-# Base64-encodes the Personal Access Token (PAT) appropriately
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "", $PAT)))
-
-Write-Host "The URL for this call: " $URL
-
-# Get the variable group by id
 $groupVariables = Invoke-RestMethod -Uri $URL -Method Get -Headers @{Authorization=("Bearer {0}" -f $PAT)}
-
-Write-Host "Variables call result: " $groupVariables.variables
-
-# Update the necessary varible values
-switch ($MODE) 
-{
-   "MAJOR" { $groupVariables.variables.Major.value = $groupVariables.variables.Major.value/1 + 1; break; }
-   "MINOR" { $groupVariables.variables.Minor.value = $groupVariables.variables.Minor.value/1 + 1; break; }
-   "REVISION" { $groupVariables.variables.Revision.value = $groupVariables.variables.Revision.value/1 + 1; break; }
-   default { throw "Something else happened";}
-}
-
-# Convert to Json
+$groupVariables.variables[$MODE].value = $groupVariables.variables[$MODE].value/1 + 1;
+# switch ($MODE) 
+# {
+   # "MAJOR" { $groupVariables.variables.Major.value = $groupVariables.variables.Major.value/1 + 1; break; }
+   # "MINOR" { $groupVariables.variables.Minor.value = $groupVariables.variables.Minor.value/1 + 1; break; }
+   # "REVISION" { $groupVariables.variables.Revision.value = $groupVariables.variables.Revision.value/1 + 1; break; }
+   # default { throw "Something else happened";}
+# }
 $json = ($groupVariables | ConvertTo-Json -Compress).ToString()
-      
-Write-Host "After modifying and Jsonifying the variables: " $json
-
-# Send the updated varible group back to Azure DevOps
 $pipeline = Invoke-RestMethod -Uri $URL -Method Put -Body $json -ContentType "application/json" -Headers @{Authorization=("Bearer {0}" -f $PAT)}
-
-Write-Host "Updating variable group was successful."
